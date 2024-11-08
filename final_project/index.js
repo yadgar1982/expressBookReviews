659762a9451b34a241a73fd65a36f2c2985a05d4
Z  Yadgar <140777972+yadgar1982@users.noreply.github.com>
2024-11-08 01:03:42 -0500

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -4,6 +4,7 @@ const session = require('express-session')
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
+
 const app = express();
 
 app.use(express.json());
@@ -12,9 +13,20 @@ app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUni
 
 app.use("/customer/auth/*", function auth(req,res,next){
 //Write the authenication mechanism here
+    let user=req.session.user;
+    const data=req.body;
+    if(user.username==data.username){
+       
+        res.send("user is logged in successfully")
+        // next()
+    }
+    else
+    {
+        res.send("invalid credentials")
+    }
 });
  
-const PORT =5000;
+const PORT =6000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
