d237cc746a497ebc54b15bf9b7c051cad837286e
Z  Yadgar <140777972+yadgar1982@users.noreply.github.com>
2024-11-08 01:02:03 -0500

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -2,11 +2,14 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
 const regd_users = express.Router();
+const session=require("express-session");
 
 let users = [];
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
+
+
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
@@ -15,14 +18,50 @@ const authenticatedUser = (username,password)=>{ //returns boolean
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
+
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+ const data= req.body;
+ const isUsername= users.find(item=>item.username==data.username);
+ if(isUsername)
+ {
+  if(isUsername.password==data.password)
+  {
+    const token=jwt.sign(isUsername,"12344",{expiresIn:"1h"});
+    req.session.user = {token,username:isUsername.username};
+    console.log(req.session.user)
+    return res.status(200).json({message: "Log in success", token});
+
+
+
+  }
+  else
+  {
+    return res.status(404).json({message: "wrong password"})
+  }
+ }
+ else 
+ {
+  return res.status(404).json({message: "User name not found"})
+ }
+
+  
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn=req.params.isbn;
+  const user=req.session.user;
+  const isUser=users.find(item=>item.username==users.username)
+  if(isUser)
+  {
+    return res.status(300).json({message: "Valid User"});
+  }
+  else
+  {
+    return res.status(401).json({message: "unathenticated user"});
+  }
+  
 });
 
 module.exports.authenticated = regd_users;
